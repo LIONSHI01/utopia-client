1b581abdff179fc4bd4b8dcc445908dfed502843
Lion Shi <shizijian2010@yahoo.com.hk>
2023-01-04 09:28:02 +0800

    modify reat-toastify bar width and position on mobile device

--- components/DisplayList/index.styles.js
+++ components/DisplayList/index.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { device } from '../../styles/devices';
 
 export const DisplayContainer = styled.main`
   width: 100%;
@@ -7,6 +8,12 @@ export const DisplayContainer = styled.main`
   justify-items: center;
   gap: 3rem;
   margin: 0 auto;
+
+  @media ${device.mobileL} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
   .pagination_box {
     grid-column: 1/-1;
