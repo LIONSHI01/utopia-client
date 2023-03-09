5a4f7c3ae6dcce341816d73e4b6d019dccb50765
Lion Shi <shizijian2010@yahoo.com.hk>
2023-03-09 11:42:31 +0800

    add GTM tag

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
         <QueryClientProvider client={queryClient}>
           <ThemeProvider theme={themeMode}>
             <Layout theme={theme} themeToggler={themeToggler}>
+              <script>
+                {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-PN668BW');
+`}
+              </script>
+
               <Component {...pageProps} />
               <ToastContainer
                 position="bottom-right"
